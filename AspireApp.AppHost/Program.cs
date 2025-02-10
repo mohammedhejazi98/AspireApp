17d768a765e179c453b303a75c9262f41af00a24
Mohammed Hejazi <mohd_tariq_hejazi@hotmail.com>
2025-02-10 13:08:27 +0300

    ..

--- AspireApp.AppHost/Program.cs
+++ AspireApp.AppHost/Program.cs
@@ -5,17 +5,17 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 
 var prometheus = builder.AddContainer("prometheus", "prom/prometheus")
-    .WithBindMount("prometheus", "/etc/prometheus", isReadOnly: true)
+    .WithBindMount("../AspireApp.ApiService/wwwroot/prometheus", "/etc/prometheus", isReadOnly: true)
     .WithArgs("--web.enable-otlp-receiver", "--config.file=/etc/prometheus/prometheus.yml")
     .WithHttpEndpoint(targetPort: 9090, name: "http");
 
 var grafana = builder.AddContainer("grafana", "grafana/grafana")
-    .WithBindMount("grafana/config", "/etc/grafana", isReadOnly: true)
-    .WithBindMount("grafana/dashboards", "/var/lib/grafana/dashboards", isReadOnly: true)
+    .WithBindMount("../AspireApp.ApiService/wwwroot/grafana/config", "/etc/grafana", isReadOnly: true)
+    .WithBindMount("../AspireApp.ApiService/wwwroot/grafana/dashboards", "/var/lib/grafana/dashboards", isReadOnly: true)
     .WithEnvironment("PROMETHEUS_ENDPOINT", prometheus.GetEndpoint("http"))
     .WithHttpEndpoint(targetPort: 3000, name: "http");
 
-builder.AddOpenTelemetryCollector("otelcollector", "otelcollector/config.yaml")
+builder.AddOpenTelemetryCollector("otelcollector", "../AspireApp.ApiService/wwwroot/otelcollector/config.yaml")
     .WithEnvironment("PROMETHEUS_ENDPOINT", $"{prometheus.GetEndpoint("http")}/api/v1/otlp");
 
 
